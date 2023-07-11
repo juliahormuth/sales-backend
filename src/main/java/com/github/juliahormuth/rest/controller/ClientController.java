8b0e3ecdae769b625780973432e2e1853dda4821
Julia Hormuth <jvhormuth@gmail.com>
2023-07-10 21:39:14 -0300

    fixed controller with @restController

--- src/main/java/com/github/juliahormuth/rest/controller/ClientController.java
+++ src/main/java/com/github/juliahormuth/rest/controller/ClientController.java
@@ -10,11 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping("/api/clients")
 public class ClientController {
 
     @Autowired
@@ -24,48 +26,51 @@ public class ClientController {
         this.clientRepository = clientRepository;
     }
 
-    @GetMapping("/api/clients/{id}")
-    @ResponseBody // transform Client to Json
-    public ResponseEntity getClientById(@PathVariable Integer id) {
-      Optional<Client> clients = clientRepository.findById(id);
-      if (clients.isPresent()) {
-          return ResponseEntity.ok(clients.get());
-      }
-      return ResponseEntity.notFound().build();
+    @GetMapping("{id}")
+    // @GetMapping("/api/clients/{id}") just do see the old form
+   // @ResponseBody // transform Client to Json
+    public Client getClientById(@PathVariable Integer id) {
+      return clientRepository.findById(id)
+              .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                      "Client not found"));
     }
 
-    @PostMapping("/api/clients")
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Client client) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+  //  @ResponseBody
+    public Client save(@RequestBody Client client) {
         Client clientCreated = clientRepository.save(client);
-        return ResponseEntity.ok(clientCreated);
+        return clientRepository.save(client);
     }
 
-    @DeleteMapping("/api/clients/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Integer id) {
-       Optional<Client> client = clientRepository.findById(id);
-
-       if(client.isPresent()) {
-           clientRepository.delete(client.get());
-           return ResponseEntity.noContent().build(); // sucesso sem return
-       }
-        return ResponseEntity.notFound().build();
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    // @ResponseBody
+    public void delete(@PathVariable Integer id) {
+        clientRepository.findById(id)
+                .map(client -> { clientRepository.delete(client);
+                      return client;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Client not found"));
     }
 
-    @PutMapping("/api/clients/{id}")
-    @ResponseBody
-    public ResponseEntity update(@PathVariable Integer id, @RequestBody Client client) {
-         return clientRepository.findById(id).map( clientExists -> {
-             client.setId(clientExists.getId());
-             clientRepository.save(client);
-             return ResponseEntity.noContent().build();
-         }).orElseGet(() -> ResponseEntity.notFound().build());
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+  //  @ResponseBody
+    public void update(@PathVariable Integer id, @RequestBody Client client) {
+         clientRepository.findById(id)
+                 .map(clientExists -> {
+                     client.setId(clientExists.getId());
+                     clientRepository.save(client);
+                     return clientExists;
+                 }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                         "Client not found"));
     }
 
-    @GetMapping("/api/clients")
-    @ResponseBody
-    public ResponseEntity find (Client filter) {
+    @GetMapping
+    // @ResponseBody
+    public List<Client> find (Client filter) {
         ExampleMatcher matcher = ExampleMatcher
                 .matching()
                 .withIgnoreCase()
@@ -73,7 +78,6 @@ public class ClientController {
 
         // find with key params
         Example example = Example.of(filter, matcher);
-        List<Client> clients = clientRepository.findAll(example);
-        return ResponseEntity.ok(clients);
+        return clientRepository.findAll(example);
     }
 }
